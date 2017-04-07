51bc5b2abed4ae0c59414b845c522e8f1f4c3041
Damian Legawiec <damian@sparksolutions.co>
2017-04-07 09:31:28 +0200

    Add vendor_id to tables that going to be vendorized

---
+++ db/migrate/20170406102944_add_vendor_id_to_spree_models.rb
@@ -0,0 +1,16 @@
+class AddVendorIdToSpreeModels < ActiveRecord::Migration
+  def change
+    table_names = %w[
+      option_types
+      properties
+      products
+      stock_locations
+      shipping_methods
+      variants
+    ]
+
+    table_names.each do |table_name|
+      add_reference "spree_#{table_name}", :vendor, index: true
+    end
+  end
+end
