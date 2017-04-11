f6ffae1012f42650454b689958a747c4cd3db667
symarys <maria.syczewska@sparksolutions.co>
2017-04-11 12:38:00 +0200

    Add state machine to vendor model

--- app/models/spree/vendor.rb
+++ app/models/spree/vendor.rb
@@ -1,5 +1,15 @@
 module Spree
   class Vendor < Spree::Base
     validates :name, presence: true, uniqueness: true
+
+    state_machine :state, initial: :pending do
+      event :activate do
+        transition to: :active
+      end
+
+      event :block do
+        transition to: :blocked
+      end
+    end
   end
 end
