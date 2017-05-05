ee73c32c7b1e1e2dc6fcdc047e000f5a5b943dea
symarys <maria.syczewska@sparksolutions.co>
2017-05-05 10:13:05 +0200

    Add scopes to product property

--- app/models/spree/product_property_decorator.rb
+++ app/models/spree/product_property_decorator.rb
@@ -3,6 +3,9 @@ Spree::ProductProperty.class_eval do
 
   before_validation :set_vendor_id, unless: proc { property.vendor_id }
 
+  scope :with_vendor, -> (vendor_id) { joins(:property).where(spree_properties: { vendor_id: vendor_id }) }
+  scope :with_name, -> (name) { joins(:property).where(spree_properties: { name: name }) }
+
   private
 
   def set_vendor_id
