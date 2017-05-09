9318b5819be5a61a982284fe2b638afedae6d2dc
symarys <maria.syczewska@sparksolutions.co>
2017-05-09 13:29:41 +0200

    Require vendor factories in lib

--- lib/spree_multi_vendor/factories.rb
+++ lib/spree_multi_vendor/factories.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
-  # Define your Spree extensions Factories within this file to enable applications, and other extensions to use and override them.
-  #
-  # Example adding this to your spec_helper will load these Factories for use:
-  # require 'spree_multi_vendor/factories'
+  Dir[Dir.pwd + '/spec/factories/**/*.rb'].each do |factory|
+    require File.expand_path(factory)
+  end
 end
