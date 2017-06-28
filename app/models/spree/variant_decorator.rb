28e44bb71007dbd31f1d6bd09585c2ef86eefa85
symarys <maria.syczewska@sparksolutions.co>
2017-06-28 11:25:43 +0200

    Create stock items only for vendor locations

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -6,4 +6,10 @@ Spree::Variant.class_eval do
   def assign_vendor_id
     self.vendor_id = product.vendor_id
   end
+
+  def create_stock_items
+    Spree:: StockLocation.where(propagate_all_variants: true, vendor_id: vendor_id).each do |stock_location|
+      stock_location.propagate_variant(self)
+    end
+  end
 end
