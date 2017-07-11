4606ff7d3fa72a4315dd3b8caa3ab25095067c39
symarys <maria.syczewska@sparksolutions.co>
2017-07-11 11:49:45 +0200

    Delete associated objects when vendor is deleted

--- app/models/spree/vendor.rb
+++ app/models/spree/vendor.rb
@@ -4,13 +4,16 @@ module Spree
 
     validates :name, presence: true, uniqueness: { case_sensitive: false }
 
-    has_many :option_types
-    has_many :products
-    has_many :properties
-    has_many :shipping_methods
-    has_many :stock_locations
-    has_many :variants
-    has_many :vendor_users
+    with_options dependent: :destroy do
+      has_many :option_types
+      has_many :products
+      has_many :properties
+      has_many :shipping_methods
+      has_many :stock_locations
+      has_many :variants
+      has_many :vendor_users
+    end
+
     has_many :users, through: :vendor_users
 
     after_create :create_stock_location
