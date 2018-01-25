30fb54d408c8553612d6749abc5178fb23c04820
Artem Kozaev <artemkozaev@gmail.com>
2018-01-25 14:35:07 +0300

    Fixes 'Passing a class...' deprecation warning

--- app/models/spree/vendor_user.rb
+++ app/models/spree/vendor_user.rb
@@ -1,7 +1,7 @@
 module Spree
   class VendorUser < Spree::Base
-    belongs_to :vendor, class_name: Spree::Vendor
-    belongs_to :user, class_name: Spree.user_class
+    belongs_to :vendor, class_name: 'Spree::Vendor'
+    belongs_to :user, class_name: Spree.user_class.to_s
 
     validates :vendor_id, uniqueness: { scope: :user_id }
   end
