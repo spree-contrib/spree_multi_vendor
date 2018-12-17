9c5116e3f220d66689a71f9995778c2cdddf9347
Damian Legawiec <damian@sparksolutions.co>
2018-12-17 09:53:13 +0100

    Bump version to 0.1.0

--- lib/spree_multi_vendor/version.rb
+++ lib/spree_multi_vendor/version.rb
@@ -9,9 +9,9 @@ module SpreeMultiVendor
 
   module VERSION
     MAJOR = 0
-    MINOR = 0
-    TINY  = 1
-    PRE   = 'alpha'.freeze
+    MINOR = 1
+    TINY  = 0
+    PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
