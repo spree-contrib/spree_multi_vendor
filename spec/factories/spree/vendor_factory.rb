6d7387c26285d2edddfdbc76e010f30f3ed70ee3
Damian Legawiec <damian@sparksolutions.co>
2018-12-17 09:43:38 +0100

    Fixed FactoryBot deprecations

--- spec/factories/spree/vendor_factory.rb
+++ spec/factories/spree/vendor_factory.rb
@@ -1,15 +1,15 @@
 FactoryBot.define do
   factory :vendor, class: Spree::Vendor do
-    name 'Test vendor'
+    name { 'Test vendor' }
 
     factory :active_vendor do
-      name 'Active vendor'
-      state :active
+      name { 'Active vendor' }
+      state { :active }
     end
 
     factory :blocked_vendor do
-      name 'Blocked vendor'
-      state :blocked
+      name { 'Blocked vendor' }
+      state { :blocked }
     end
   end
 end
