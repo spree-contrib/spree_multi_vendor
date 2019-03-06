1c3bd0d89219840d65b44118caf3e19881e2a54d
Robert Nowakowski <aplegatt@gmail.com>
2019-03-06 09:59:17 +0100

    [HEMP-12] Adds commission rate managing for super admin

--- spec/features/spree/admin/vendor_spec.rb
+++ spec/features/spree/admin/vendor_spec.rb
@@ -31,6 +31,7 @@ RSpec.feature 'Admin Vendors', :js do
       fill_in 'vendor_name', with: 'Test'
       fill_in 'vendor_about_us', with: 'About...'
       fill_in 'vendor_contact_us', with: 'Contact...'
+      expect(find_field('vendor_commission_rate').value).to eq '5.0'
       if Spree.version.to_f >= 3.6
         page.attach_file("vendor_image", Spree::Core::Engine.root + 'spec/fixtures' + 'thinking-cat.jpg')
       end
@@ -76,6 +77,7 @@ RSpec.feature 'Admin Vendors', :js do
       fill_in 'vendor_name', with: 'Testing edit'
       fill_in 'vendor_about_us', with: 'Testing about us'
       fill_in 'vendor_contact_us', with: 'Testing contact us'
+      fill_in 'vendor_commission_rate', with: '5.5'
       click_button 'Update'
       expect(page).to have_text 'successfully updated!'
       expect(page).to have_text 'Testing edit'
