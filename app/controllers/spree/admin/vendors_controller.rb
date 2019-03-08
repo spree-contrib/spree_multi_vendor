0f12a368f6153ec9483865c6ef9ed92fec0b6ff6
Robert Nowakowski <aplegatt@gmail.com>
2019-03-08 12:50:22 +0100

    Adds vendors sorting by priority for admin

--- app/controllers/spree/admin/vendors_controller.rb
+++ app/controllers/spree/admin/vendors_controller.rb
@@ -16,6 +16,17 @@ module Spree
         super
       end
 
+      def update_positions
+        params[:positions].each do |id, position|
+          vendor = Spree::Vendor.find(id)
+          vendor.set_list_position(position)
+        end
+
+        respond_to do |format|
+          format.js { render plain: 'Ok' }
+        end
+      end
+
       private
 
       def find_resource
@@ -24,7 +35,7 @@ module Spree
 
       def collection
         params[:q] = {} if params[:q].blank?
-        vendors = super.order(name: :asc)
+        vendors = super.order(priority: :asc)
         @search = vendors.ransack(params[:q])
 
         @collection = @search.result.
