1187c3b801dee73ed4e976c5cd7504adc3e45f65
Piotr Milewski <piotr.milewski@sparksolutions.co>
2019-03-18 11:29:37 +0100

    Vendorize order total, subtotal

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,3 +1,9 @@
-Spree::LineItem.class_eval do
-  scope :for_vendor, ->(vendor) { includes(:product).where('spree_products.vendor_id' => vendor.id) }
+module SpreeMultiVendor
+  module LineItemDecorator
+    def self.prepended(base)
+      base.scope :for_vendor, ->(vendor) { includes(:product).where('spree_products.vendor_id' => vendor.id) }
+      base.scope :not_for_vendor, ->(vendor) { includes(:product).where.not('spree_products.vendor_id' => vendor.id) }
+    end
+  end
 end
+Spree::LineItem.prepend SpreeMultiVendor::LineItemDecorator
