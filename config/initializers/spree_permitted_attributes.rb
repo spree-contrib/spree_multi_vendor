83a4c28169bf243853f5c4e2035b7c7337f6cbad
Robert Nowakowski <aplegatt@gmail.com>
2019-03-22 15:46:03 +0100

    Fixes vendor order notification email errors

--- config/initializers/spree_permitted_attributes.rb
+++ config/initializers/spree_permitted_attributes.rb
@@ -4,7 +4,7 @@ module Spree
 
     mattr_reader *ATTRIBUTES
 
-    @@vendor_attributes = [:name, :about_us, :contact_us]
+    @@vendor_attributes = [:name, :about_us, :contact_us, :notification_email]
     @@vendor_attributes << :image if Spree.version.to_f >= 3.6
   end
 end
