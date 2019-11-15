3ac73626f79c0a8ee14bc177eabbee660437b81d
usmanriaz111 <l124061@lhr.nu.edu.pk>
2019-11-15 20:57:39 +0500

    updated the view of sidebar

--- app/overrides/spree/admin/layouts/admin.rb
+++ app/overrides/spree/admin/layouts/admin.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
   virtual_path:  'spree/layouts/admin',
   name:          'vendors_main_menu_tabs',
-  insert_bottom: '#main-sidebar',
+  insert_bottom: '#custom_vendor_tab',
   text:       <<-HTML
                 <% if current_spree_user.respond_to?(:has_spree_role?) && current_spree_user.has_spree_role?(:admin) %>
                   <ul class="nav nav-sidebar">
