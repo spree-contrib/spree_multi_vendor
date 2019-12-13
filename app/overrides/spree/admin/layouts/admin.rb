4cf02cf23a0ad3114dacb6d1f18ad795f62ee325
usmanriaz111 <49909779+usmanriaz111@users.noreply.github.com>
2019-12-13 15:10:57 +0500

    Update admin.rb
    updted the override

--- app/overrides/spree/admin/layouts/admin.rb
+++ app/overrides/spree/admin/layouts/admin.rb
@@ -4,12 +4,12 @@ Deface::Override.new(
   insert_bottom: '#custom_vendor_tab',
   text:       <<-HTML
                 <% if current_spree_user.respond_to?(:has_spree_role?) && current_spree_user.has_spree_role?(:admin) %>
-                  <ul class="nav nav-sidebar">
+                  <ul class="nav nav-sidebar border-bottom">
                     <%= tab plural_resource_name(Spree::Vendor), url: admin_vendors_path, icon: 'money' %>
                   </ul>
                 <% end %>
                 <% if defined?(current_spree_vendor) && current_spree_vendor %>
-                  <ul class="nav nav-sidebar">
+                  <ul class="nav nav-sidebar border-bottom">
                     <%= tab Spree::Vendor.model_name.human, url: admin_vendor_settings_path, icon: 'money' %>
                   </ul>
                 <% end %>
