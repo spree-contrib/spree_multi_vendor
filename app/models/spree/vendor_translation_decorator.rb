3c0145a2386eb26f2a7ac08db7cf3178afff763b
Daniel <daniel.uremix@gmail.com>
2020-04-17 14:29:52 -0400

    Modifications for decorator

--- app/models/spree/vendor_translation_decorator.rb
+++ app/models/spree/vendor_translation_decorator.rb
@@ -1,12 +1,10 @@
 module Spree::VendorDecorator
-    def self.prepended(base)
-      if defined? (SpreeGlobalize)
-        base.translates :name, :about_us, :contact_us, :slug, fallbacks_for_empty_translations: true
-      end
+  def self.prepended(base)
+    if defined? (SpreeGlobalize)
+      base.translates :name, :about_us, :contact_us, :slug, fallbacks_for_empty_translations: true
     end
-
-    Spree::Vendor.include SpreeGlobalize::Translatable if defined?(SpreeGlobalize)
   end
-end
 
+  Spree::Vendor.include SpreeGlobalize::Translatable if defined?(SpreeGlobalize)
+end
 ::Spree::Vendor.prepend(Spree::VendorDecorator) if defined? (SpreeGlobalize)
