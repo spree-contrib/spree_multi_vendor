9eff1e6ec0f42989028f4948e932e0f1f3f77836
Tomek Urban <tomasz.urban@sparksolutions.co>
2020-05-19 17:13:08 +0200

    Upgrade to Spree 4.1
    * replace development dependencies occuring in https://github.com/spree/spree_dev_tools/blob/master/spree_dev_tools.gemspec with 'spree_dev_tools'
    * update Travis CI configuration
    
    Upgrade to Spree 4.1
    
    * replace development dependencies occuring in https://github.com/spree/spree_dev_tools/blob/master/spree_dev_tools.gemspec with 'spree_dev_tools'
    * update Travis CI configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,44 +1,12 @@
-require 'simplecov'
-SimpleCov.start 'rails'
+# Configure Rails Environment
+ENV['RAILS_ENV'] = 'test'
 
-ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../dummy/config/environment.rb', __FILE__)
 
-begin
-  require File.expand_path('../dummy/config/environment', __FILE__)
-rescue LoadError
-  puts 'Could not load dummy application. Please ensure you have run `bundle exec rake test_app`'
-  exit
-end
-
-require 'pry'
-require 'ffaker'
-require 'factory_bot'
-require 'selenium/webdriver'
-require 'rspec/rails'
 require 'shoulda/matchers'
-require 'capybara-select-2'
+require 'spree_dev_tools/rspec/spec_helper'
 
-include Warden::Test::Helpers
-Warden.test_mode!
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].sort.each { |f| require f }
 
-RSpec.configure do |config|
-  config.mock_with :rspec
-  config.use_transactional_fixtures = false
-  config.fail_fast = false
-  config.filter_run focus: true
-  config.run_all_when_everything_filtered = true
-
-  config.infer_spec_type_from_file_location!
-  config.raise_errors_for_deprecations!
-  config.include CapybaraSelect2
-
-  config.expect_with :rspec do |expectations|
-    expectations.syntax = :expect
-  end
-
-  config.before :each do
-    Rails.cache.clear
-  end
-end
-
-Dir[File.join(File.dirname(__FILE__), '/support/**/*.rb')].each { |file| require file }
