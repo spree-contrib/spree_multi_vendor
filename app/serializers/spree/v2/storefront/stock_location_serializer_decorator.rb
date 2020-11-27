c15ca5d3e27758815bb096573c3485e2c911f652
chhlga <youzik@me.com>
2020-11-27 16:33:51 +0100

    Apply suggestions from code review
    Co-authored-by: Damian Legawiec <damian@sparksolutions.co>

--- app/serializers/spree/v2/storefront/stock_location_serializer_decorator.rb
+++ app/serializers/spree/v2/storefront/stock_location_serializer_decorator.rb
@@ -1,13 +1,13 @@
-module Spree
+module SpreeMultiVendor
   module V2
     module Storefront
       module StockLocationSerializerDecorator
         def self.prepended(base)
-          base.belongs_to :vendor, serializer: Spree::V2::Storefront::VendorSerializer
+          base.belongs_to :vendor, serializer: ::Spree::V2::Storefront::VendorSerializer
         end
       end
     end
   end
 end
 
-Spree::V2::Storefront::StockLocationSerializer.prepend(Spree::V2::Storefront::StockLocationSerializerDecorator)
+Spree::V2::Storefront::StockLocationSerializer.prepend(SpreeMultiVendor::V2::Storefront::StockLocationSerializerDecorator)
