aeb7939b7420eb0c54fd63ef62b90444fbd345b8
Damian Legawiec <damian@sparksolutions.co>
2021-02-09 23:51:16 +0100

    More API v2 tests

--- spec/requests/spree/api/v2/storefront/products_spec.rb
+++ spec/requests/spree/api/v2/storefront/products_spec.rb
@@ -52,4 +52,42 @@ describe 'API V2 Storefront Products Spec', type: :request do
       end
     end
   end
+
+  describe 'products#show' do
+    let(:product) { products.first }
+
+    context 'with existing product' do
+      before { get "/api/v2/storefront/products/#{product.slug}?include=vendor" }
+
+      it_behaves_like 'returns 200 HTTP status'
+
+      it 'returns a valid JSON response' do
+        expect(json_response['data']).to have_id(product.id.to_s)
+
+        expect(json_response['data']).to have_type('product')
+
+        expect(json_response['data']).to have_attribute(:name).with_value(product.name)
+        expect(json_response['data']).to have_attribute(:description).with_value(product.description)
+        expect(json_response['data']).to have_attribute(:price).with_value(product.price.to_s)
+        expect(json_response['data']).to have_attribute(:currency).with_value(product.currency)
+        expect(json_response['data']).to have_attribute(:display_price).with_value(product.display_price.to_s)
+        expect(json_response['data']).to have_attribute(:available_on).with_value(product.available_on.as_json)
+        expect(json_response['data']).to have_attribute(:slug).with_value(product.slug)
+        expect(json_response['data']).to have_attribute(:meta_description).with_value(product.meta_description)
+        expect(json_response['data']).to have_attribute(:meta_keywords).with_value(product.meta_keywords)
+        expect(json_response['data']).to have_attribute(:updated_at).with_value(product.updated_at.as_json)
+        expect(json_response['data']).to have_attribute(:purchasable).with_value(product.purchasable?)
+        expect(json_response['data']).to have_attribute(:in_stock).with_value(product.in_stock?)
+        expect(json_response['data']).to have_attribute(:backorderable).with_value(product.backorderable?)
+
+        expect(json_response['data']).to have_relationships(
+          :variants, :option_types, :product_properties, :default_variant, :vendor
+        )
+
+        expect(json_response['included'].first).to have_id(product.vendor_id.to_s)
+        expect(json_response['included'].first).to have_attribute(:name).with_value(product.vendor.name)
+        expect(json_response['included'].first).to have_attribute(:about_us).with_value(product.vendor.about_us)
+      end
+    end
+  end
 end
