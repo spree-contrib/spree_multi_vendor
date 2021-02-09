0d5ddfd65cd45e1ac4c6bea524d359079783bdc1
Damian Legawiec <damian@sparksolutions.co>
2021-02-09 10:25:18 +0100

    Only set Vendor ID if supported by model

--- app/controllers/spree_multi_vendor/spree/admin/resource_controller_decorator.rb
+++ app/controllers/spree_multi_vendor/spree/admin/resource_controller_decorator.rb
@@ -1,6 +1,8 @@
 module SpreeMultiVendor::Spree::Admin::ResourceControllerDecorator
   def set_vendor_id
-    return unless current_spree_vendor
+    return if current_spree_vendor.blank? || resource.nil?
+    return unless resource.respond_to?(:vendor_id)
+
     params[resource.object_name.to_sym][:vendor_id] = current_spree_vendor.id
   end
 end
