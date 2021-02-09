01a092cff29e5e61d6de7fbfd5666c8fee2ff55c
Damian Legawiec <damian@sparksolutions.co>
2021-02-09 23:51:40 +0100

    Additional check is vendor method defined

--- app/models/spree_multi_vendor/spree/stock/package_decorator.rb
+++ app/models/spree_multi_vendor/spree/stock/package_decorator.rb
@@ -1,7 +1,8 @@
 module SpreeMultiVendor::Spree::Stock::PackageDecorator
   def shipping_methods
-    vendor = stock_location.vendor
-    
+    vendor = Spree::StockLocation.method_defined?(:vendor) ? stock_location.vendor : nil
+
+    # return only shipping methods for this Vendor
     if vendor && Spree::ShippingMethod.method_defined?(:vendor)
       vendor.shipping_methods.to_a
     else
