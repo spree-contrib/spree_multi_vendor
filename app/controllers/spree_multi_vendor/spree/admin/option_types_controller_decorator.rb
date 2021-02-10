4676cb68fda1352805203f373f5f2bc025459da4
Damian Legawiec <damian@sparksolutions.co>
2021-02-10 19:23:32 +0100

    Removed OptionType/Properties decorators

--- app/controllers/spree_multi_vendor/spree/admin/option_types_controller_decorator.rb
+++
@@ -1,7 +0,0 @@
-module SpreeMultiVendor::Spree::Admin::OptionTypesControllerDecorator
-  def self.prepended(base)
-    base.before_action :set_vendor_id, only: [:create, :update]
-  end
-end
-
-Spree::Admin::OptionTypesController.prepend SpreeMultiVendor::Spree::Admin::OptionTypesControllerDecorator
--- app/controllers/spree_multi_vendor/spree/admin/properties_controller_decorator.rb
+++
@@ -1,7 +0,0 @@
-module SpreeMultiVendor::Spree::Admin::PropertiesControllerDecorator
-  def self.prepended(base)
-    base.before_action :set_vendor_id, only: [:create, :update]
-  end
-end
-
-Spree::Admin::PropertiesController.prepend SpreeMultiVendor::Spree::Admin::PropertiesControllerDecorator
