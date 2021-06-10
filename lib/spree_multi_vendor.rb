3272cd50b4c43d9683e99a48ead87cc36be25112
omairrazam <omairrazam@users.noreply.github.com>
2021-06-10 19:07:12 +0500

    namespace correction

--- lib/spree_multi_vendor.rb
+++ lib/spree_multi_vendor.rb
@@ -8,7 +8,7 @@ module SpreeMultiVendor
   # TODO: this should be moved into preferences
   def self.vendorized_models
     SpreeMultiVendor::Config[:vendorized_models].map(&:classify).map do |class_name|
-      "Spree::#{class_name}".safe_constantize
+      "::Spree::#{class_name}".safe_constantize
     end.compact.uniq
   end
 end
