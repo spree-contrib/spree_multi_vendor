d01c01cd87287010d1de1a5cd742785691e21d95
Mohammed Amarnah <m.amarnah@gmail.com>
2021-10-20 07:46:42 -0500

    fix irreversible migrations (#204)

--- db/migrate/20210728121024_change_vendor_id_and_order_id_type_for_spree_order_commissions.rb
+++ db/migrate/20210728121024_change_vendor_id_and_order_id_type_for_spree_order_commissions.rb
@@ -1,8 +1,14 @@
 class ChangeVendorIdAndOrderIdTypeForSpreeOrderCommissions < ActiveRecord::Migration[4.2]
-  def change
+  def up
     change_table(:spree_order_commissions) do |t|
       t.change :vendor_id, :bigint
       t.change :order_id, :bigint
+    end
+  end
+  def down
+    change_table(:spree_order_commissions) do |t|
+      t.change :vendor_id, :integer
+      t.change :order_id, :integer
     end
   end
 end
