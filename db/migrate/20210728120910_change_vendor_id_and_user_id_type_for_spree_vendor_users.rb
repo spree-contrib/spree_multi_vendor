67688275d6e969f64376ca78f843613cf609ab10
Mohammed Amarnah <m.amarnah@gmail.com>
2021-10-20 07:32:32 -0500

    fix irreversible migrations

--- db/migrate/20210728120910_change_vendor_id_and_user_id_type_for_spree_vendor_users.rb
+++ db/migrate/20210728120910_change_vendor_id_and_user_id_type_for_spree_vendor_users.rb
@@ -1,8 +1,14 @@
 class ChangeVendorIdAndUserIdTypeForSpreeVendorUsers < ActiveRecord::Migration[4.2]
-  def change
+  def up
     change_table(:spree_vendor_users) do |t|
       t.change :vendor_id, :bigint
       t.change :user_id, :bigint
+    end
+  end
+  def down
+    change_table(:spree_vendor_users) do |t|
+      t.change :vendor_id, :integer
+      t.change :user_id, :integer
     end
   end
 end
