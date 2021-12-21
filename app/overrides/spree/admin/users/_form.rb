8e0b3858daf0ba621d0958d42d6f813522510ccd
hoshino tsuyoshi <tsuyoshi.hoshino@route06.co.jp>
2021-12-21 18:01:03 +0900

    Fix: remove redundant `Spree.t` (#211)

--- app/overrides/spree/admin/users/_form.rb
+++ app/overrides/spree/admin/users/_form.rb
@@ -5,7 +5,7 @@ Deface::Override.new(
   text: <<-HTML
           <% if current_spree_user.respond_to?(:has_spree_role?) && current_spree_user.has_spree_role?(:admin) %>
             <%= f.field_container :vendor_ids, class: ['form-group'] do %>
-              <%= f.label :vendor_ids, Spree.t(plural_resource_name(Spree::Vendor)) %>
+              <%= f.label :vendor_ids, plural_resource_name(Spree::Vendor) %>
               <%= f.collection_select(:vendor_ids, Spree::Vendor.all, :id, :name, { }, { class: 'select2', multiple: true }) %>
             <% end %>
           <% end %>
